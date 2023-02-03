2b7092f4815c2882dec91ca601e773e4ea50d553
dom0000D <dominick.ferraro001@studenti.uniparthenope.it>
2023-02-03 10:31:57 +0100

    Expandend intents.json Add comments in nltk_utils.py

--- nltk_utils.py
+++ nltk_utils.py
@@ -5,35 +5,34 @@ stemmer = SnowballStemmer("italian")
 
 def tokenize(sentence):
     """
-    split sentence into array of words/tokens
-    a token can be a word or punctuation character, or number
+    divide la frase in un array di parole
     """
     return nltk.word_tokenize(sentence)
 
 
 def stem(word):
     """
-    stemming = find the root form of the word
-    examples:
-    words = ["organize", "organizes", "organizing"]
-    words = [stem(w) for w in words]
-    -> ["organ", "organ", "organ"]
+    stemming = trovare la radice di una parola
+    esempi:
+    parole = ["computer", "computare", "computabile", "computazionale"]
+    parole = [stem(x) for x in parole]
+    -> ["comput", "comput", "comput","comput"]
     """
     return stemmer.stem(word.lower())
 
 
 def bag_of_words(tokenized_sentence, words):
     """
-    return bag of words array:
-    1 for each known word that exists in the sentence, 0 otherwise
-    example:
-    sentence = ["hello", "how", "are", "you"]
-    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
-    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    ritorna un array di bag of words:
+    assegna 1 per ogni parola conosciuta che esiste nella frase, se la parola non esiste assegna 0
+    esempio:
+    frase = ["oggi", "é", "bel", "tempo"]
+    words = ["ciao", "oggi", "é", "proprio", "bel", "tempo"]
+    bog   = [  0 ,     1 ,    1 ,     0 ,      1 ,     1 ]
     """
-    # stem each word
+    # stem ogni parola
     sentence_words = [stem(word) for word in tokenized_sentence]
-    # initialize bag with 0 for each word
+    # inizializzo la bag a 0 per ogni parola
     bag = np.zeros(len(words), dtype=np.float32)
     for idx, w in enumerate(words):
         if w in sentence_words: 
